0e940d76ce3ded26ab0dbc65c73911e41d236ab2
tunahanyetimoglu <tunahan.yetimoglu@bil.omu.edu.tr>
2017-06-08 21:09:09 +0300

    Create ortalama.java
    try-catch exception eklenecek!

---
+++ Denemeler/ortalama.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exam;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.Action;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Tunahan
+ */
+public class ortalama extends JFrame {
+    static JButton bt1,bt2;
+    static JTextField tf1,tf2;
+    static JLabel lb1,lb2,lb3,lb4;
+    
+    ortalama(){
+        setTitle("Ortalama Hesapla");
+        setBounds(0,0,480,350);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+        
+        tf1 = new JTextField("");
+        tf2 = new JTextField("");
+        
+        tf1.setBounds(80,25,50,25);
+        tf2.setBounds(80,65,50,25);
+        add(tf1);
+        add(tf2);
+        
+      
+        
+        lb1 = new JLabel("Vize Notu: ");
+        lb2 = new JLabel("Final Notu: ");
+        lb3 = new JLabel("Ortalama :");
+        lb4 = new JLabel("");
+        
+        lb1.setBounds(10,20,60,30);
+        lb2.setBounds(10,60,80,30);
+        lb3.setBounds(10,100,80,30);
+        lb4.setBounds(90,100,80,30);
+        add(lb1);
+        add(lb2);
+        add(lb3);
+        add(lb4);
+        
+        bt1 = new JButton("Hesapla");
+        bt2 = new JButton("Temizle");
+        bt2.setEnabled(false);
+        
+        bt1.setBounds(150,23,100,30);
+        bt2.setBounds(150,63,100,30);
+        add(bt1);
+        add(bt2);
+        
+        setVisible(true);
+        
+        bt1.addActionListener(new ActionListener(){
+          
+           
+            public void actionPerformed(ActionEvent e) {
+                
+                double ortalama = Double.parseDouble(tf1.getText())*0.4 + Double.parseDouble(tf2.getText())*0.6;
+                lb4.setText(""+ortalama);
+                bt2.setEnabled(true);
+                bt1.setEnabled(false);
+                tf1.setEditable(false);
+                tf2.setEditable(false);             
+               
+            }
+        });
+        
+        bt2.addActionListener(new ActionListener(){
+           public void actionPerformed(ActionEvent e){
+               lb4.setText("");
+               bt1.setEnabled(true);
+               bt2.setEnabled(false);
+               tf1.setEditable(true);
+               tf2.setEditable(true);
+               tf1.setText("");
+               tf2.setText("");
+           } 
+        });
+    }
+    
+    public static void main(String args[]){
+        new ortalama();
+    }
+}
